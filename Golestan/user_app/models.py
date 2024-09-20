234cf68b4caca4eb7b477a26ca0fc92dcd9b5588
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-20 10:14:58 +0330

    user completed

--- Golestan/user_app/models.py
+++ Golestan/user_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_id = models.CharField(max_length=20)
+    lessons = models.ManyToManyField('unit_app.Lesson', blank=True)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    teacher_id = models.CharField(max_length=20)
+
