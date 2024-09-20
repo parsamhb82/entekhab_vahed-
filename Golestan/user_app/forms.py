f001d71bb8f442f20beaa1d90fe7ec8c07fd4d3e
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-20 10:32:53 +0330

    user form

--- Golestan/user_app/forms.py
+++ Golestan/user_app/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import *
 
-
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
