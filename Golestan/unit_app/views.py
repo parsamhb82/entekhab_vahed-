eeb46ba7484e1095d072384bbf2fcf4bed1343eb
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-20 10:49:06 +0330

    teachers view

--- Golestan/unit_app/views.py
+++ Golestan/unit_app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import rende
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Lesson
+from .forms import LessonForm
+
+@login_required
+def create_lesson(request):
+    if request.method == 'POST':
+        form = LessonForm(request.POST)
+        if form.is_valid():
+            lesson = form.save(commit=False)
+            lesson.teacher = request.user.teacher  # Assign the lesson to the logged-in teacher
+            lesson.save()
+            return redirect('lesson_list')  # Redirect to lesson list after successful creation
+    else:
+        form = LessonForm()
+    return render(request, 'lesson_form.html', {'form': form})
+
+def edit_lesson(request, pk):
+    lesson = get_object_or_404(Lesson,pk=pk, teacher=request.user.teacher)
+    if request.method == 'POST':
+        form = LessonForm(request.POST, instance=lesson)
+        if form.is_valid():
+            form.save()
+            return redirect('lesson_list')
+    else:
+        form = LessonForm(instance=lesson)
+    return render(request, 'lesson_form.html', {'form': form})
+
+def delete_lesson(request, pk):
+    lesson = get_object_or_404(Lesson, pk=pk, teacher=request.user.teacher)
+    lesson.delete()
+    return redirect('lesson_list')
+
